4758b2bc38a7ae7e2a8d5c68b7efdc8a6b83a6c1
Luis Alfonso Talegón Puente <luisetex@gmail.com>
2018-10-16 02:00:45 +0200

    work done on screen_grabber module, adapted to new scheme

--- screenshot_handler/screen_grabber.py
+++ screenshot_handler/screen_grabber.py
@@ -7,7 +7,7 @@ from keyboard_handler.keyboard_reader import get_pressed_key
 class ScreenGrabber:
     def __init__(self, dest_folder, monitor=None):
         self.dest_folder = dest_folder+'/'
-        self.monitor = monitor if monitor is None else __parse_monitor(monitor)
+        self.monitor = monitor if monitor is None else parse_monitor(monitor)
         self.sct = mss.mss()
         self.sct.compression_level = 0
 
@@ -19,17 +19,17 @@ class ScreenGrabber:
         mss.tools.to_png(sct_img.rgb, sct_img.size, level=0, output=output_path)
 
     def grab_screenshot(self, index):
-        output_path = self.dest_folder + __get_screenshot_name(index)
+        output_path = self.dest_folder + get_screenshot_name(index)
         if self.monitor is None:
             self.__grab_fullscreen(output_path)
         else:
             self.__grab_part_screen(output_path)
 
-def __parse_monitor(monitor):
+def parse_monitor(monitor):
     position, size = monitor.split('_')
-    top, left = position.split('x')[0], position.split('x')[1]
-    width, height = size.split('x')[0], size.split('x')[1]
+    top, left = int(position.split('x')[0]), int(position.split('x')[1])
+    width, height = int(size.split('x')[0]), int(size.split('x')[1])
     return {'top': top, 'left': left, 'width': width, 'height': height}
 
-def __get_screenshot_name(index):
+def get_screenshot_name(index):
     return index + get_pressed_key()
