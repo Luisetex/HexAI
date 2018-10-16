c7e5b4477d2840b8403e5b80d4450d4366b3e595
Luis Alfonso Talegón Puente <luisetex@gmail.com>
2018-10-16 02:00:53 +0200

    finish image_transformer

--- image_operations/image_transformer.py
+++ image_operations/image_transformer.py
@@ -1,7 +1,25 @@
+import os
+
 import cv2
 
-def rgb_to_grayscale(image_path):
+def rgb_to_grayscale(image):
+    grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return grayscale_image
+
+def shrink_image(image, scale=4):
+    scale = 1/scale
+    shrinked_image = cv2.resize(
+        image,
+        None,
+        fx=scale,
+        fy=scale,
+        interpolation=cv2.INTER_AREA
+    )
+    return shrinked_image
+
+def preprocess_image(image_path, scale, dest_folder):
     folder, image_name = image_path.split('/')
-    original_image = cv2.imread(image_path)
-    grayscale_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite('_'.join((folder, 'gray', image_name)), grayscale_image)
+    original_image = cv2.imread(os.path.abspath(image_path))
+    grayscaled = rgb_to_grayscale(original_image)
+    shrinked = shrink_image(grayscaled, scale)
+    cv2.imwrite('/'.join((dest_folder, image_name)), shrinked)
